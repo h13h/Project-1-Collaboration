541fb2427d4363e0be44a73ee36ff4f55c177975
h13h <harshal.patel1305@gmail.com>
2025-03-12 14:13:00 -0400

    new commit

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files (CSS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Routes for all 10 pages
 app.get('/', (req, res) => res.render('home'));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/portfolio', (req, res) => res.render('portfolio'));
+app.get('/contact', (req, res) => res.render('contact'));
 app.get('/services', (req, res) => res.render('services'));
 app.get('/blog', (req, res) => res.render('blog'));
 app.get('/testimonials', (req, res) => res.render('testimonials'));
-app.get('/contact', (req, res) => res.render('contact'));
 app.get('/pricing', (req, res) => res.render('pricing'));
 app.get('/faq', (req, res) => res.render('faq'));
-app.get('/privacy', (req, res) => res.render('privacy'));
+app.get('/privacy-policy', (req, res) => res.render('privacy-policy'));
 
 // Start server
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
