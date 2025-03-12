eb55caedaaca18b69b8c95a2a1580ae050698a4b
h13h <harshal.patel1305@gmail.com>
2025-03-12 13:34:17 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './views/**/*.ejs',  // Ensure EJS files are included
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
