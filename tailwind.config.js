31ea07e9c2fc4488c5010b6832c4320772ae10d0
h13h <harshal.patel1305@gmail.com>
2025-03-12 16:02:35 -0400

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    './views/**/*.ejs',
-    './public/**/*.{html,js}',
+    './views/**/*.ejs',  // This will include all EJS files in the views folder
+    './public/**/*.{html,js}',  // Ensure that public HTML/JS files are also covered
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
